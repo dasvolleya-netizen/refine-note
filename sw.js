6c2f68424659bd18eee03f854d6b370a59a78500
dasvolleya-netizen <das.volley.a@gmail.com>
2026-08-15 19:02:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+// Refine Note v0.5
+// 開発中は古い画面が残らないよう、キャッシュを使用しません。
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})()));
+self.addEventListener('fetch',()=>{});
